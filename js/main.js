6173b3e7f7ef331eae4d9d58517faab694c2b94b
Jenny Lo <ajmoser@Jennys-MacBook-Air.local>
2017-06-25 15:23:47 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
-cardOne = cards[0];
-cardsInPlay.push (cardOne);
-console.log("User flipped " + cards[1]);
-var cardTwo;
-cardTwo = cards[2];
-cardsInPlay.push (cardTwo);
-console.log("User flipped " + cardOne + " and " + cardTwo);
-if (cardsInPlay.length === 2) {
-console.log("You have flipped 2 cards");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("No match try again");
+	}
 }
-if (cardsInPlay[0] === cardsInPlay [1]) {alert("You found a match!");
-} 
-else {alert("No match try again");
+checkForMatch();
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[CardId]);
+	cardsInPlay.push(cards[cardId]);
+flipCard();
+flipCard(0);
+flipCard(2);
+
 }
 
+	
