1de365c1829100fc5ec2c8653c49bb5b9923b33a
Jenny Lo <ajmoser@Jennys-MacBook-Air.local>
2017-06-25 17:09:25 -0700

    createed game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
 
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ 
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "mages/queen-of-hears.png" 
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+	];
 var cardsInPlay = [];
 var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay [1]) {
 		console.log("You found a match!");
 	} else {
 		console.log("No match try again");
 	}
+	}
 }
 checkForMatch();
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[CardId]);
-	cardsInPlay.push(cards[cardId]);
-flipCard();
-flipCard(0);
-flipCard(2);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	flipCard();
+	flipCard(0);
+	flipCard(2);
 
 }
 
